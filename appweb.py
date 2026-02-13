3b1004f2378fcb969126a92f530a160be8816a3d
RatoMestre1998 <ratosplinter1998@gmail.com>
2026-02-13 14:56:00 +0000

    Update appweb.py

--- appweb.py
+++ appweb.py
@@ -6,7 +6,7 @@ import streamlit as st
 desafios = [
     {
         "titulo": "Step 1: Configure R1 Basic Settings and Device Hardening (Part A)",
-        "instrucoes: Configure basic settings.": [
+        "instrucoes:": [
             "A. Configure basic settings.",
             "- Prevent the router from attempting to resolve incorrectly entered commands as domain names.",
             "- Configure the R1 hostname.",
